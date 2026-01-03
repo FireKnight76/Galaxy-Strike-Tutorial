25342fcbcee9a47309c7417e91794abed1cbc550
FireKnight76 <fireknight593@gmail.com>
2026-01-03 16:19:47 +0100

    Added a scoreboard and added points to the targets

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,12 +3,33 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject explosionParticles;
+    [SerializeField] float hitPoints = 3f;
+    [SerializeField] int scorePoints = 1;
+
+    Scoreboard scoreboard;
+
+    private void Start()
+    {
+        scoreboard = FindFirstObjectByType<Scoreboard>();
+    }
 
     private void OnParticleCollision(GameObject other)
     {
-        Instantiate(explosionParticles, transform.position, Quaternion.identity);
-        Destroy(gameObject); 
-        //var explosion = explosionParticles.GetComponent<ParticleSystem>().emission;
-        //explosion.enabled = true;
+        ProcessHits();
+
     }
+
+    void ProcessHits()
+    {
+        hitPoints--;
+
+        if (hitPoints == 0)
+        {
+            scoreboard.IncreaseScore(scorePoints);
+            Instantiate(explosionParticles, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+
+        }
+    }
+
 }
